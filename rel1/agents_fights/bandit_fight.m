fe9e6ba842d827490a89616d52d6e109d7ac9db2
gmarseglia <g.marseglia.it@gmail.com>
2024-03-18 18:49:30 +0100

    #5 && close #6

--- rel1/agents_fights/bandit_fight.m
+++ rel1/agents_fights/bandit_fight.m
@@ -3,15 +3,17 @@ function [r1, r2] = bandit_fight(agent1_int, agent2_int)
 agent1_int = agent1_int - 1;
 agent2_int = agent2_int - 1;
     
-if agent2_int == mod(agent1_int + 2, 3)
+if agent2_int == mod(agent1_int + 2, 5) || agent2_int == mod(agent1_int + 4, 5)
         r1 = 1;
         r2 = -1;
-    elseif agent2_int == mod(agent1_int + 1, 3)
+    elseif agent2_int == mod(agent1_int + 1, 5) || agent2_int == mod(agent1_int + 3, 5)
         r1 = -1;
         r2 = 1;
     else 
-        r1 = 0;
-        r2 = 0;
+        % r1 = 0;
+        r1 = -0.5;
+        % r2 = 0;
+        r2 = -0.5;
 end
 
 end
