56eb9bc256ac7b55bf0d79f842a9e2e721e00639
Alessandro <alessandro.finocchi@students.uniroma2.eu>
2024-03-31 11:59:09 +0200

    Developing a plot manager to destroy tecnical debt

--- rel1/agents_fights/UCB_vs_preference.m
+++ rel1/agents_fights/UCB_vs_preference.m
@@ -23,13 +23,15 @@ historyN1 = zeros(A, lengthEpisode); % save history of N
 alpha2 = 4e-5; % update step for preferences
 beta2 = 1e-2; % update step for rewards
 H2 = zeros(A, 1); % preferences of actions
+N2 = zeros(A, 1); % number of times we take each action
 avg_r2 = 0; % initialization of average reward 
 historyH2 = zeros(A, lengthEpisode); % save history of H
+historyN2 = zeros(A, lengthEpisode); % save history of N
 
 for i = 1:lengthEpisode
     
     %-----------------------UCB agent work-------------------------
-    Qext1 = Q1 + c*sqrt(log(i)./(N1+1)); % extended value function
+    Qext1 = Q1 + c1*sqrt(log(i)./(N1+1)); % extended value function
     % we choose the action that maximized the Qext
     agent_int1 = find(Qext1 == max(Qext1)); 
     agent_int1 = agent_int1(randi(length(agent_int1))); % parity broken by random
@@ -48,6 +50,7 @@ for i = 1:lengthEpisode
    
     %---------Update preferences for the preference update agent---------
     H2(agent_int2) = H2(agent_int2) + alpha2*(r2 - avg_r2)*(1-Proba2(agent_int2)); 
+    N2(agent_int2) = N2(agent_int2) + 1;
     % update not-taken actions preferences
     notagent_int2 = 1:A;
     notagent_int2(agent_int2) = [];
@@ -60,6 +63,7 @@ for i = 1:lengthEpisode
 
     %---------save the history for the preference update agent----------
     historyH2(:,i) = H2;
+    historyN2(:,i) = N2;
     
     % ---------Update match result history---------------------
     if r1 == 1
@@ -75,16 +79,24 @@ for i = 1:lengthEpisode
 end
 
 %% plots
+figure;
+
+figure('Position', [0, 50, , ]); % Impostare la posizione e le dimensioni della finestra [x, y, larghezza, altezza]
+
+
+
 
 %-----------------plot the history of N------------------------------------
 % Fixed
-figure('Position', [0 0 1280 720])
+figure('Position', [0 50 700 420])
 hold on
 % Graph content
 title('N')
 plot(historyN1', 'LineWidth', 1.5)
 plot(historyN2', 'LineWidth', 1.5, 'LineStyle','--')
-lgn = legend('Rock_1', 'Paper_1', 'Scissors_1', 'Spock_1', 'Lizard_1', 'Rock_2', 'Paper_2', 'Scissors_2', 'Spock_2', 'Lizard_2');
+lgn = legend( ...
+    'Rock_1', 'Paper_1', 'Scissors_1', 'Spock_1', 'Lizard_1', ...
+    'Rock_2', 'Paper_2', 'Scissors_2', 'Spock_2', 'Lizard_2');
 set(gca, 'ColorOrder', colors(5))
 % Fixed
 grid on
@@ -96,13 +108,12 @@ saveas(gcf, "graphs/UCB_vs_UCB/N", "png")
 
 %-----------------plot the history of Q------------------------------------
 % Fixed
-figure('Position', [0 0 1280 720])
+figure('Position', [700 50 700 420])
 hold on
 % Graph content
-title('Q')
+title('Q1')
 plot(historyQ1', 'LineWidth', 1.5)
-plot(historyQ2', 'LineWidth', 1.5, 'LineStyle','--')
-lgn = legend('Rock_1', 'Paper_1', 'Scissors_1', 'Spock_1', 'Lizard_1', 'Rock_2', 'Paper_2', 'Scissors_2', 'Spock_2', 'Lizard_2');
+lgn = legend('Rock_1', 'Paper_1', 'Scissors_1', 'Spock_1', 'Lizard_1');
 set(gca, 'ColorOrder', colors(5))
 % Fixed
 grid on
@@ -112,9 +123,25 @@ hold off
 saveas(gcf, "graphs/UCB_vs_UCB/Q", "png")
 %--------------------------------------------------------------------------
 
+%-----------------------plot the history of H1-----------------------------
+% Fixed
+figure('Position', [0 550 700 420])
+hold on
+% Graph content
+title('H2')
+plot(historyH2', 'LineWidth', 1.5)
+lgn = legend('Rock_1', 'Paper_1', 'Scissors_1', 'Spock_1', 'Lizard_1');
+set(gca, 'ColorOrder', colors(5))
+% Fixed
+grid on
+lgn.Location = 'northeastoutside';
+hold off
+% Save graph
+saveas(gcf, "graphs/pref_vs_pref/H", "png")
+
 %-----------------plot the history of W------------------------------------
 % Fixed
-figure('Position', [0 0 1280 720])
+figure('Position', [700 550 700 420])
 hold on
 % Graph content
 title('W')
