d731384577955094a732e59fb41eb4deb3b24d74
Alessandro <alessandro.finocchi@students.uniroma2.eu>
2024-03-31 15:23:33 +0200

    #16, #17: finished implementing, still to check correctness

--- rel1/agents_fights/e_greedy_vs_UCB.m
+++ rel1/agents_fights/e_greedy_vs_UCB.m
@@ -19,14 +19,13 @@ N1 = zeros(A, 1); % number of times we take each action
 historyQ1 = zeros(A, lengthEpisode); % save history of Q
 historyN1 = zeros(A, lengthEpisode); % save history of N
 
-%----------Initialization for the preference update agent----------
-alpha2 = 4e-5; % update step for preferences
-beta2 = 1e-2; % update step for rewards
-H2 = zeros(A, 1); % preferences of actions
-N2 = zeros(A, 1); % number of times we take each action
-avg_r2 = 0; % initialization of average reward 
-historyH2 = zeros(A, lengthEpisode); % save history of H
-historyN2 = zeros(A, lengthEpisode); % save history of N
+%--------------Initialization for the e-greedy agent--------------
+epsilon2 = 0.05;
+alpha2 = 1e-2; % step size
+Q2 = zeros(A, 1); % estimate of the value of actions for agent 1
+N2 = zeros(A, 1); % number of times we take each action for agent 1
+historyQ2 = zeros(A, lengthEpisode);
+historyN2 = zeros(A, lengthEpisode); 
 
 for i = 1:lengthEpisode
     
@@ -36,10 +35,12 @@ for i = 1:lengthEpisode
     agent_int1 = find(Qext1 == max(Qext1)); 
     agent_int1 = agent_int1(randi(length(agent_int1))); % parity broken by random
     
-    %----------------Preference update agent work------------------  
-    Proba2 = exp(H2)/sum(exp(H2)); % Compute softmax for each action
-    csProba2 = cumsum(Proba2); %choose action based on softmax
-    agent_int2 = find(rand < csProba2, 1, "first");
+    %---------------------e-greedy agent work----------------------  
+    if rand < epsilon2
+        agent_int2 = randi(A); % we take a random action
+    else
+        agent_int2 = find(Q2 == max(Q2), 1, 'first'); % we take the greedy action
+    end
 
     [r1, r2] = bandit_fight(agent_int1, agent_int2); % compute reward
 
@@ -49,22 +50,17 @@ for i = 1:lengthEpisode
 
    
     %---------Update preferences for the preference update agent---------
-    H2(agent_int2) = H2(agent_int2) + alpha2*(r2 - avg_r2)*(1-Proba2(agent_int2)); 
     N2(agent_int2) = N2(agent_int2) + 1;
-    % update not-taken actions preferences
-    notagent_int2 = 1:A;
-    notagent_int2(agent_int2) = [];
-    H2(notagent_int2) = H2(notagent_int2) - alpha2*(r2 - avg_r2)*Proba2(notagent_int2);
-    avg_r2 = avg_r2 + beta2*(r2-avg_r2); % update average reward with fixed step
+    Q2(agent_int2) = Q2(agent_int2) + alpha2*(r2 - Q2(agent_int2));
 
     %----------------save the history for the UCB agent-----------------
     historyQ1(:, i) = Q1;
     historyN1(:, i) = N1;
 
     %---------save the history for the preference update agent----------
-    historyH2(:,i) = H2;
+    historyQ2(:,i) = Q2;
     historyN2(:,i) = N2;
-    
+
     % ---------Update match result history---------------------
     if r1 == 1
         W = W + [1, 0, 0];
@@ -97,7 +93,7 @@ grid on
 lgn.Location = 'northeastoutside';
 hold off
 % Save graph
-saveas(gcf, "graphs/UCB_vs_pref/N", "png")
+saveas(gcf, "graphs/e_vs_UCB/N", "png")
 %--------------------------------------------------------------------------
 
 %-----------------plot the history of Q------------------------------------
@@ -105,37 +101,24 @@ saveas(gcf, "graphs/UCB_vs_pref/N", "png")
 figure('Position', [700 50 700 420])
 hold on
 % Graph content
-title('Q1')
+title('Q')
 plot(historyQ1', 'LineWidth', 1.5)
-lgn = legend('Rock_1', 'Paper_1', 'Scissors_1', 'Spock_1', 'Lizard_1');
+plot(historyQ2', 'LineWidth', 1.5, 'LineStyle','--')
+lgn = legend( ...
+    'Rock_1', 'Paper_1', 'Scissors_1', 'Spock_1', 'Lizard_1', ...
+    'Rock_2', 'Paper_2', 'Scissors_2', 'Spock_2', 'Lizard_2');
 set(gca, 'ColorOrder', colors(5))
 % Fixed
 grid on
 lgn.Location = 'northeastoutside';
 hold off
 % Save graph
-saveas(gcf, "graphs/UCB_vs_pref/Q", "png")
+saveas(gcf, "graphs/e_vs_UCB/Q", "png")
 %--------------------------------------------------------------------------
-
-%-----------------------plot the history of H1-----------------------------
-% Fixed
-f=figure('Position', [0 550 700 420])
-hold on
-% Graph content
-title('H2')
-plot(historyH2', 'LineWidth', 1.5)
-lgn = legend('Rock_1', 'Paper_1', 'Scissors_1', 'Spock_1', 'Lizard_1');
-set(gca, 'ColorOrder', colors(5))
-% Fixed
-grid on
-lgn.Location = 'northeastoutside';
-hold off
-% Save graph
-saveas(gcf, "graphs/UCB_vs_pref/H", "png")
 
 %-----------------plot the history of W------------------------------------
 % Fixed
-figure('Position', [700 550 700 420])
+figure('Position', [0 550 700 420])
 hold on
 % Graph content
 title('W')
@@ -149,5 +132,5 @@ grid on
 lgn.Location = 'northeastoutside';
 hold off
 % Save
-saveas(gcf, "graphs/UCB_vs_pref/W", "png")
+saveas(gcf, "graphs/e_vs_UCB/W", "png")
 %--------------------------------------------------------------------------
